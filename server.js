d486baadcb12b7076600c0e636bab0aadaec98d8
Jeremy Polanco Lara <jeremyinf26@gmail.com>
2022-07-19 10:25:18 -0400

    fix content security policy

--- server.js
+++ server.js
@@ -34,7 +34,11 @@ if (process.env.NODE_ENV !== 'production') {
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(path.resolve(__dirname, './client/build')));
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 app.use(xss());
 app.use(mongoSanitize());
 
